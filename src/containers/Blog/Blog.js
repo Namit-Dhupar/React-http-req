9f9fc88104838ba7b99cdb69cc069007da36ecb7
Namit Dhupar <33388782+GitWithMe@users.noreply.github.com>
2020-01-23 11:58:00 +0530

    route

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,54 +1,35 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+// import axios from 'axios';
+import { Route, NavLink } from 'react-router-dom'; //Enable Routing and NavLink is used as anchor tab for react apps
 import './Blog.css';
+import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
-    
-    state = {
-        posts: [],
-        selectedPostID: null
-    }
-    //This lifecycle is best for http requests as HTTP
-    //Requests are Side Effects
-    componentDidMount(){
-      axios.get('https://jsonplaceholder.typicode.com/posts')
-      .then(response => {
-        const fourposts = response.data.slice(0, 4); //Get only first 4 responses
-        const updatedPosts = fourposts.map(mypost => {
-            return {
-                ...mypost,
-                author: 'Namit'
-            }
-        });
-         this.setState({posts:updatedPosts});
-      });
-    }
-
-    postSelectedHandler = (id) =>{
-       this.setState({selectedPostID: id});
-    } 
     render () {
-        const jsonposts = this.state.posts.map(mypost => {
-          return <Post 
-          title={mypost.title} 
-          key={mypost.id} 
-          author={mypost.author}
-          clicked={()=>this.postSelectedHandler(mypost.id)/*Gets the ID of post clicked*/}/>;
-        });
         return (
-            <div>
-                <section className="Posts">
-                   {jsonposts}
-                </section>
-                <section>
-                    <FullPost postid={this.state.selectedPostID} />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><NavLink to='/' exact>Home</NavLink></li>
+                            <li><NavLink to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</NavLink></li>
+
+                        </ul>
+                    </nav>
+                </header>
+                {/*Here this tag is useful for routing and self explainatory
+                <Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/newpost" render={() => <h1>Home 2</h1>} /> */}
+                
+                <Route path='/' exact component={Posts} />
+                <Route path='/new-post' exact component={NewPost} />
+                <Route path='/:id' exact component={FullPost} /> {/*For Dynamic ID's*/}
             </div>
         );
     }
